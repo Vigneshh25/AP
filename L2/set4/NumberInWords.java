713311026f4717520235db9349d079b3aed851da
Vicky <81636126+Vigneshh25@users.noreply.github.com>
2023-03-04 16:09:15 +0530

    Update NumberInWords.java

--- L2/set4/NumberInWords.java
+++ L2/set4/NumberInWords.java
@@ -1,6 +1,8 @@
 package set4;
 
-class numbersinwords {
+
+
+class NumbersInWords {
     public static final String[] NUMBERS = {
             "zero", "one", "two", "three", "four",
             "five", "six", "seven", "eight", "nine",
@@ -14,35 +16,47 @@ class numbersinwords {
         if (n == 0) {
             return NUMBERS[0];
         }
-        int thousands = n / 1000;
-        int hundreds = (n % 1000) / 100;
+
+        int lakhs = n / 100000;
+        int thousands = (n / 1000) % 100;
+        int hundreds = (n / 100) % 10;
         int tens = (n % 100) / 10;
         int units = n % 10;
         String s = "";
+
+        if (lakhs > 0) {
+            s += toWords(lakhs) + " lakh ";
+        }
+
         if (thousands > 0) {
-            s += NUMBERS[thousands] + " thousand and ";
+            s += toWords(thousands) + " thousand ";
         }
+
         if (hundreds > 0) {
-            s += NUMBERS[hundreds] + " hundred and ";
+            s += NUMBERS[hundreds] + " hundred ";
         }
+
         if (tens > 0) {
             if (tens == 1) {
                 s += NUMBERS[10 + units] + " ";
             } else {
                 s += NUMBERS[20 + tens - 2] + " ";
                 if (units > 0) {
-                    s += "and " + NUMBERS[units] + " ";
+                    s += NUMBERS[units] + " ";
                 }
             }
         } else if (units > 0) {
             s += NUMBERS[units] + " ";
         }
+
         return s.trim();
     }
 
     public static void main(String[] args) {
-        System.out.println(toWords(234));
-        System.out.println(toWords(1234));
-        System.out.println(toWords(12345));
+        System.out.println(toWords(123445));
+        System.out.println(toWords(999999));
+        System.out.println(toWords(456));
+
+        System.out.println(toWords(0));
     }
 }
