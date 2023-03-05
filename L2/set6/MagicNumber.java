d6c1959286858988bdbf0ba87b0d9288560dceba
Vicky <81636126+Vigneshh25@users.noreply.github.com>
2023-03-05 22:46:13 +0530

    Update MagicNumber.java

--- L2/set6/MagicNumber.java
+++ L2/set6/MagicNumber.java
@@ -1,21 +1,16 @@
 package set6;
 
 public class MagicNumber {
-    public static void main(String[] args) {
-        int number = 124;
-        boolean isMagic = isMagicNumber(number);
-        System.out.println(isMagic);
-    }
-    public static boolean isMagicNumber(int number) {
-        int cubed = number * number * number;
-        while (number > 0) {
-            int digit = number % 10;
-            if (cubed % 10 != digit) {
-                return false;
-            }
-            number /= 10;
-            cubed /= 10;
-        }
-        return true;
-    }
+   public static void main(String[] args) 
+{
+      
+    // Accepting sample input
+    int x = 1234;
+  
+    // Condition to check Magic number
+    if (x % 9 == 1)
+        System.out.printf("Magic Number");
+    else
+        System.out.printf("Not a Magic Number");
+}
 }
