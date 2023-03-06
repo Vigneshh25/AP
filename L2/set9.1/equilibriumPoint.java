410c9c59feddb538fcda3e1cf78575dab35adc66
Vicky <81636126+Vigneshh25@users.noreply.github.com>
2023-03-06 23:01:58 +0530

    Create equilibriumPoint.java

---
+++ L2/set9.1/equilibriumPoint.java
@@ -0,0 +1,15 @@
+ public static int equilibriumPoint(long arr[], int n) {
+
+        // Your code here
+        long sum =0;
+        long leftsum =0;
+        sum = Arrays.stream(arr).sum();
+        for(int i=0;i<n;i++)
+        {
+            sum -=arr[i];
+            if(sum==leftsum)
+                return i+1;
+            leftsum +=arr[i];
+        }
+        return -1;
+    }
