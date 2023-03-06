90f878ddcc4f449a9b359d16afd5c55cb765ed59
Vicky <81636126+Vigneshh25@users.noreply.github.com>
2023-03-06 22:49:51 +0530

    Create booleanMatrix.java

---
+++ L2/set9.1/booleanMatrix.java
@@ -0,0 +1,27 @@
+void booleanMatrix(int mat[][])
+    {
+        // code here 
+         int[] row = new int[mat.length];
+         int[] col = new int[mat[0].length];
+         
+         for(int i=0;i<mat.length;i++)
+         {
+             for(int j=0;j<mat[0].length;j++){
+             
+                 if(mat[i][j]==1)
+                 {
+                     row[i] =1;
+                     col[j] =1;
+                 }
+             }
+         }
+         for(int i=0;i<mat.length;i++)
+         {
+             for(int j=0;j<mat[0].length;j++)
+             {
+                 if(row[i]==1||col[j]==1)
+                    mat[i][j] =1;
+             }
+         }
+        
+    }
