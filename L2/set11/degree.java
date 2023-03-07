54f7b6e0195833a360a783571671e0a2be0b1c75
Vicky <81636126+Vigneshh25@users.noreply.github.com>
2023-03-07 17:29:01 +0530

    Update degree.java

--- L2/set11/degree.java
+++ L2/set11/degree.java
@@ -5,9 +5,67 @@ class degree
 
     static int N = 4;
 
+    public static void horizontalFlip(int[][] imgArray)
+    {
+        int temp;
+        for (int i = 0; i < imgArray.length; i++) {
+            for (int j = 0; j < imgArray[i].length/2; j++) {
+                temp = imgArray[i][j];
+                imgArray[i][j] = imgArray[i][imgArray[i].length - 1 - j];
+                imgArray[i][imgArray[i].length - 1 - j] = temp;
+            }
+        }
+    }
+
+    private void flipHorizontally() {
+        for (int y = 0; y < totalY; y++) {
+            for (int x = 0; x < totalX/2; x++) {
+                String tmp = fields[totalX-x-1][y].getText();
+                fields[totalX-x-1][y].setText(fields[x][y].getText());
+                fields[x][y].setText(tmp);
+            }
+        }
+    }
+
+
+    private void flipVertically() {
+        for (int x = 0; x < totalX; x++) {
+            for (int y = 0; y < totalY/2; y++) {
+                String tmp = fields[x][totalY - y - 1].getText();
+                fields[x][totalY - y - 1].setText(fields[x][y].getText());
+                fields[x][y].setText(tmp);
+            }
+        }
+    }
+
     // Function to rotate the matrix 90 degree clockwise
     static void rotate90Clockwise(int a[][])
     {
+
+
+//        for(int i=0;i<N;i++)
+//        {
+//            for (int j = i+1; j < N; j++)
+//            {
+//                int temp = a[i][j];
+//                a[i][j] = a[j][i];
+//                a[j][i] = temp;
+//            }
+//        }
+//        for(int i=0;i<N;i++)
+//        {
+//            int l = 0;
+//            int h = N-1;
+//            while (l<h)
+//            {
+//                int temp = a[i][h];
+//                a[i][h] = a[i][l];
+//                a[i][l] = temp;
+//                l++;
+//                h--;
+//            }
+//        }
+
 
         // Traverse each cycle
         for (int i = 0; i < N / 2; i++)
