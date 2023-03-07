ec93f55ced500393f9758da7df1f06a2af7d279d
Vicky <81636126+Vigneshh25@users.noreply.github.com>
2023-03-07 16:55:03 +0530

    Update Matrix.java

--- L2/set11/Matrix.java
+++ L2/set11/Matrix.java
@@ -1,38 +1,41 @@
 package set11;
 
 public class Matrix {
-        public static void main(String[] args) {
-            int[][] big = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-            int[][] small = {{6, 7}, {10, 11}};
-            boolean result = isSubmatrix(big, small);
-            System.out.println(result);  // Expected output: true
+    public static void main(String[] args) {
+        int[][] big = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        int[][] small = {{6, 7}, {10, 11}};
+        boolean result = isSubmatrix(big, small);
+        System.out.println(result);  // Expected output: true
 
 
-            System.out.println(result);  // Expected output: false
-        }
+        System.out.println(result);  // Expected output: false
+    }
 
-        public static boolean isSubmatrix(int[][] big, int[][] small) {
-            int n = big.length;
-            int m = small.length;
-            for (int i = 0; i < n - m + 1; i++) {
-                for (int j = 0; j < n - m + 1; j++) {
-                    boolean found = true;
-                    for (int p = 0; p < m; p++) {
-                        for (int q = 0; q < m; q++) {
-                            if (big[i + p][j + q] != small[p][q]) {
-                                found = false;
-                                break;
-                            }
-                        }
-                        if (!found) {
+    public static boolean isSubmatrix(int[][] matrix, int[][] submatrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int subRows = submatrix.length;
+        int subCols = submatrix[0].length;
+
+        for (int i = 0; i <= rows - subRows; i++) {
+            for (int j = 0; j <= cols - subCols; j++) {
+                boolean found = true;
+                for (int k = 0; k < subRows; k++) {
+                    for (int l = 0; l < subCols; l++) {
+                        if (matrix[i + k][j + l] != submatrix[k][l]) {
+                            found = false;
                             break;
                         }
                     }
-                    if (found) {
-                        return true;
+                    if (!found) {
+                        break;
                     }
                 }
+                if (found) {
+                    return true;
+                }
             }
-            return false;
         }
+        return false;
     }
+}
