9defc4f25f8d3fe16d8006be94e9a3a21f41af6b
Vicky <81636126+Vigneshh25@users.noreply.github.com>
2023-03-07 17:44:36 +0530

    Update Pattern.java

--- L2/set11/Pattern.java
+++ L2/set11/Pattern.java
@@ -1,28 +1,14 @@
-package set11;
+public class PatternPrint {
+    public static void main(String[] args) {
+        int n = 6;
 
-public class Pattern {
-
-        public static void main(String[] args) {
-            int n = 6;
-            printPattern(n);
-        }
-
-        public static void printPattern(int n) {
-            int current = 1;
-            for (int i = 1; i < n * 2; i++) {
-                if (i <= n) {
-                    System.out.println(current);
-                    current++;
-                } else {
-                    System.out.print(current + " ");
-                    for (int j = current + 1; j < current + n; j++) {
-                        System.out.print(j + " ");
-                    }
-                    System.out.println();
-                    current += n;
-                }
+        for (int i = 1; i <= n; i++) {
+            int current = i;
+            for (int j = 0; j < n-i; j++) {
+                System.out.print(current+ "\t");
+                current +=n-j;
             }
-
+            System.out.println();
+        }
     }
-
 }
