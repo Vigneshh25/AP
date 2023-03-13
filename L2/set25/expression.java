b3869f1679eb790d9b241cf763e9fabd34f7a19e
Vicky <81636126+Vigneshh25@users.noreply.github.com>
2023-03-13 11:24:12 +0530

    Update expression.java

--- L2/set25/expression.java
+++ L2/set25/expression.java
@@ -1,80 +1,71 @@
 package set25;
 
 public class expression {
-    // Java program to evaluate a given expression
+    
+    public static double evaluate(String expr) {
+        int i = 0;
+        double operand1 = 0, operand2 = 0, result = 0;
+        char operator = ' ';
 
-
-        // A utility function to check if
-// a given character is operand
-        static boolean isOperand(char c)
-        {
-            return (c >= '0' && c <= '9');
-
+        // Extract the first operand
+        while (i < expr.length() && Character.isDigit(expr.charAt(i))) {
+            operand1 = operand1 * 10 + (expr.charAt(i) - '0');
+            i++;
         }
 
-        // utility function to find value of and operand
-        static int value(char c)
-        {
-            return (int)(c - '0');
+        // Extract the operators and operands and apply them in order
+        while (i < expr.length()) {
+            operator = expr.charAt(i);
+            i++;
 
-        }
-
-        // This function evaluates simple expressions.
-// It returns -1 if the given
-// expression is invalid.
-        static int evaluate(String exp)
-        {
-            // Base Case: Given expression is empty
-            if (exp.length() == 0) return -1;
-
-            // The first character must be
-            // an operand, find its value
-            int res = value(exp.charAt(0));
-
-            // Traverse the remaining characters in pairs
-            for (int i = 1; i<exp.length(); i += 2)
-            {
-                // The next character must be an operator, and
-                // next to next an operand
-                char opr = exp.charAt(i), opd = exp.charAt(i+1);
-
-                // If next to next character is not an operand
-                if (isOperand(opd) == false) return -1;
-
-                // Update result according to the operator
-                if (opr == '+') res += value(opd);
-                else if (opr == '-') res -= value(opd);
-                else if (opr == '*') res *= value(opd);
-                else if (opr == '/') res /= value(opd);
-
-                    // If not a valid operator
-                else				 return -1;
+            // Extract the second operand
+            operand2 = 0;
+            while (i < expr.length() && Character.isDigit(expr.charAt(i))) {
+                operand2 = operand2 * 10 + (expr.charAt(i) - '0');
+                i++;
             }
-            return res;
+
+            // Apply the operator to the operands
+            switch (operator) {
+                case '+':
+                    result = operand1 + operand2;
+                    break;
+                case '-':
+                    result = operand1 - operand2;
+                    break;
+                case '*':
+                    result = operand1 * operand2;
+                    break;
+                case '/':
+                    result = operand1 / operand2;
+                    break;
+            }
+
+            // Set the first operand to the result
+            operand1 = result;
         }
 
-        // Driver program to test above function
+        return result;
+    }
+
+    // Driver program to test above function
         public static void main(String[] args)
         {
             String expr1 = "1+2*5+3";
-            int res = evaluate(expr1);
-            if(res == -1) System.out.println(expr1+" is Invalid");
-            else	 System.out.println("Value of "+expr1+" is "+res);
+             evaluate(expr1);
+          
 
             String expr2 = "1+2*3";
-            res = evaluate(expr2);
-            if(res == -1) System.out.println(expr2+" is Invalid");
-            else		 System.out.println("Value of "+expr2+" is "+res);
+            evaluate(expr2);
+           
 
             String expr3 = "4-2+6*3";
-            res = evaluate(expr3);
-            if(res == -1) System.out.println(expr3+" is Invalid");
-            else		 System.out.println("Value of "+expr3+" is "+res);
+           evaluate(expr3);
+            
 
             String expr4 = "1++2";
-            res = evaluate(expr4);
-            if(res == -1) System.out.println(expr4+" is Invalid");
-            else		 System.out.println("Value of "+expr4+" is "+res);
+           evaluate(expr4);
+           
         }
     }
 // This code is contributed by mits
