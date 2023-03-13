93bd08675edfa554dd20ed2fd766f36fecc243c7
Vicky <81636126+Vigneshh25@users.noreply.github.com>
2023-03-13 10:51:00 +0530

    Update frequency.java

--- L2/set24/frequency.java
+++ L2/set24/frequency.java
@@ -1,60 +1,25 @@
-package set24;
 
-public class frequency {
 
-        public static void main(String[] args) {
-            int[] numbers = {1, 2, 3, 3, 4, 4, 5};
-            int target = 3;
-            int count = countOccurrences(numbers, target);
-            System.out.println(count);  // Output: 2
-        }
-        
-         int count(int[] arr, int n, int x) {
+//User function Template for Java
+
+class Solution {
+    String printString(String S, char ch, int count) {
         // code here
-        
-        
-     int count = (int) Arrays.stream(arr).filter(i -> i == x).count();
-
-        return count;
-        
-    }
-
-        public static int countOccurrences(int[] numbers, int target) {
-            int first = findFirst(numbers, target);
-            if (first == -1) {
-                return 0;
-            }
-            int last = findLast(numbers, target);
-            return last - first + 1;
-        }
-
-        public static int findFirst(int[] numbers, int target) {
-            int left = 0;
-            int right = numbers.length - 1;
-            while (left < right) {
-                int mid = left + (right - left) / 2;
-                if (numbers[mid] < target) {
-                    left = mid + 1;
-                } else {
-                    right = mid;
-                }
-            }
-            return numbers[left] == target ? left : -1;
-        }
-
-        public static int findLast(int[] numbers, int target) {
-            int left = 0;
-            int right = numbers.length - 1;
-            while (left < right) {
-                int mid = left + (right - left + 1) / 2;
-                if (numbers[mid] > target) {
-                    right = mid - 1;
-                } else {
-                    left = mid;
-                }
-            }
-            return numbers[right] == target ? right : -1;
-        }
-
-
+  int i=0,cnt=0,n=S.length();
+     boolean flag=false;
+     String ans="";
+     while(i<n){
+         if(cnt==count){
+             flag=true;
+             break;
+         };
+         if(S.charAt(i)==ch){
+             cnt++;
+         }
+         
+         i++;
+     }
+     if(!flag) return "Empty string";
+     return S.substring(i);
+}
 }
