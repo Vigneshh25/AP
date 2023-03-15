576d092f25cac3a9afe94560d7b7edd4bfc4f230
Vicky <81636126+Vigneshh25@users.noreply.github.com>
2023-03-15 08:14:14 +0530

    Update Decode.java

--- L2/set38/Decode.java
+++ L2/set38/Decode.java
@@ -1,132 +1,48 @@
-package set38;
+package set28;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
+public class NumberToString {
+    private static final char[] MAPPING = {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-public class Decode {
-    public static boolean
-    nonLower(String s)
-    {
-        // Traverse the string
-        for (int i = 0; i < s.length(); i++) {
+    public static List<String> numberToString(String digits) {
+        List<String> result = new ArrayList<>();
+        generateStrings(digits, 0, "", result);
+        return result;
+    }
 
-            // If any character is not
-            // found to be in lowerCase
-            if (!Character
-                    .isLowerCase(s.charAt(i))) {
-                return true;
+    private static void generateStrings(String digits, int index, String current, List<String> result) {
+        if (index == digits.length()) {
+            result.add(current);
+            return;
+        }
+
+        int digit = digits.charAt(index) - '0';
+        if (digit == 0) {
+            return;
+        }
+
+        // map the digit to a character and add it to the current string
+        char c = MAPPING[digit];
+        generateStrings(digits, index + 1, current + c, result);
+
+        // if there are at least two digits remaining, map the next two digits to a character and add it to the current string
+        if (index < digits.length() - 1) {
+            int nextDigit = digits.charAt(index + 1) - '0';
+            int value = digit * 10 + nextDigit;
+            if (value <= 26) {
+                c = MAPPING[value];
+                generateStrings(digits, index + 2, current + c, result);
             }
         }
-
-        return false;
     }
 
-    // Function to print the decodings
-    public static void
-    printCodes(String output[])
-    {
-        for (int i = 0; i < output.length; i++) {
-
-            // If all characters are not
-            // in lowercase
-            if (nonLower(output[i]))
-                continue;
-            System.out.println(output[i]);
+    public static void main(String[] args) {
+        String digits = "1123";
+        List<String> strings = numberToString(digits);
+        for (String s : strings) {
+            System.out.println(s);
         }
-    }
-
-    // Function to return the character
-    // corresponding to given integer
-    public static char getChar(int n)
-    {
-        return (char)(n + 96);
-    }
-
-    // Function to return the decodings
-    public static String[] getCode(
-            String str)
-    {
-        // Base case
-        if (str.length() == 0) {
-
-            String ans[] = { "" };
-            return ans;
-        }
-
-        // Recursive call
-        String output1[]
-                = getCode(str.substring(1));
-
-        // Stores the characters of
-        // two digit numbers
-        String output2[] = new String[0];
-
-        // Extract first digit and
-        // first two digits
-        int firstDigit
-                = (str.charAt(0) - '0');
-        int firstTwoDigit = 0;
-
-        if (str.length() >= 2) {
-
-            firstTwoDigit
-                    = (str.charAt(0) - '0') * 10
-                    + (str.charAt(1) - '0');
-
-            // Check if it lies in the
-            // range of alphabets
-            if (firstTwoDigit >= 10
-                    && firstTwoDigit <= 26) {
-
-                // Next recursive call
-                output2
-                        = getCode(str.substring(2));
-            }
-        }
-
-        // Combine both the output in a
-        // single final output array
-        String output[]
-                = new String[output1.length
-                + output2.length];
-
-        // Index of final output array
-        int k = 0;
-
-        // Store the elements of output1
-        // in final output array
-        for (int i = 0; i < output1.length; i++) {
-
-            char ch = getChar(firstDigit);
-
-            output[i] = ch + output1[i];
-            k++;
-        }
-
-        // Store the elements of output2
-        // in final output array
-        for (int i = 0; i < output2.length; i++) {
-
-            char ch = getChar(firstTwoDigit);
-
-            output[k] = ch + output2[i];
-            k++;
-        }
-
-        // Result the result
-        return output;
-    }
-
-    // Driver Code
-    public static void main(String[] args)
-    {
-        String input = "1123";
-
-        // Function call
-        String output[] = getCode(input);
-
-        // Print function call
-        printCodes(output);
     }
 }
