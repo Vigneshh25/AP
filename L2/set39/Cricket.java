50d82fa3ba478516bf40c60d454861cf53cc2159
Vicky <81636126+Vigneshh25@users.noreply.github.com>
2023-03-15 11:17:31 +0530

    Update Cricket.java

--- L2/set39/Cricket.java
+++ L2/set39/Cricket.java
@@ -1,46 +1,44 @@
 package set39;
 
-public class Cricket {
+public class CricketScores {
+    public static void main(String[] args) {
+        String scores = "1 . 2 . 4 3 6 W 1 . N . 2 1";
+        String[] scoreList = scores.split(" ");
 
-    public static void cricketScores(String scoreTimeline) {
         int p1Score = 0;
         int p2Score = 0;
         int extras = 0;
-        boolean isP1Batting = true;
+        boolean isP1 = true;
+        boolean isP2 = true;
 
-        for (int i = 0; i < scoreTimeline.length(); i++) {
-            char c = scoreTimeline.charAt(i);
-            if (c == 'W') {
-                extras++;
-            } else if (c == 'N') {
-                extras++;
-            } else if (c == '.') {
-                // do nothing
+        for (String score : scoreList) {
+            if (score.equals(".")) {
+                    // Dot ball, no score
+            } else if (score.equals("W")) {
+                extras += 1; // Wide ball, add 1 to Extras
+            } else if (score.equals("N")) {
+                extras += 1; // No ball, add 1 to Extras
+                ; // Free hit, add 1 to Player 2's score
             } else {
-                int score = Character.getNumericValue(c);
-                if (isP1Batting) {
-                    p1Score += score;
-                    isP1Batting = false;
-                } else {
-                    p2Score += score;
-                    isP1Batting = true;
+                int runs = Integer.parseInt(score);
+
+                if (isP1) {
+                    if(runs%2!=0) {
+                        isP1 = false;
+                    }
+                    p1Score += runs;
+                } else  {
+                    if(runs%2!=0) {
+                        isP1 = true;
+                    }
+                    p2Score += runs;
                 }
+                // Switch to other player
             }
         }
 
-        System.out.println("P1: " + p1Score);
-        System.out.println("P2: " + p2Score);
+        System.out.println("Player 1 Score: " + p1Score);
+        System.out.println("Player 2 Score: " + p2Score);
         System.out.println("Extras: " + extras);
     }
-
-    public static void main(String[] args) {
-        String scoreTimeline = "1 . 2 . 4 3 6 W 1 . N . 2 1";
-        cricketScores(scoreTimeline);
-// prints:
-// P1: 8
-// P2: 12
-// Extras: 2
-
-    }
-
 }
