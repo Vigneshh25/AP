16cfda6154c1a6f66d967c8653ff7426515e5d24
Vicky <81636126+Vigneshh25@users.noreply.github.com>
2023-03-16 15:51:13 +0530

    Create LongestPalindromeSubstring.java

---
+++ L2/set0/LongestPalindromeSubstring.java
@@ -0,0 +1,33 @@
+package set0;
+
+import java.util.Map;
+
+public class LongestPalindromeSubstring {
+    public static void main(String[] args) {
+        String s =  "cbbbccdddddaras";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        System.out.println(s.substring(start,end+1));
+
+    }
+
+    private static int expandAroundCenter(String s,int l,int r)
+    {
+        while(l>=0 && r<s.length()&&s.charAt(l)==s.charAt(r))
+        {
+            l--;
+            r++;
+        }
+        return r-l-1;
+
+    }
+
+}
