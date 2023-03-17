00d99021968901d6d32ad5de1be418d17c3e2c94
Vicky <81636126+Vigneshh25@users.noreply.github.com>
2023-03-17 11:19:12 +0530

    Update SmallNumber.java

--- L2/set0/SmallNumber.java
+++ L2/set0/SmallNumber.java
@@ -18,24 +18,18 @@ public class SmallNumber {
     static void printLargest(Vector<String> arr)
     {
 
-        Collections.sort(arr, new Comparator<String>()
-        {
-            // A comparison function which is used by
-            // sort() in printLargest()
-            @Override public int compare(String X, String Y)
-            {
+        Collections.sort(arr, (X, Y) -> {
 
-                // first append Y at the end of X
-                String XY = X + Y;
+            // first append Y at the end of X
+            String XY = X + Y;
 
-                // then append X at the end of Y
-                String YX = Y + X;
+            // then append X at the end of Y
+            String YX = Y + X;
 
-                // Now see which of the two
-                // formed numbers
-                // is greater
-                return XY.compareTo(YX) > 0 ? 1 : -1;
-            }
+            // Now see which of the two
+            // formed numbers
+            // is greater
+            return XY.compareTo(YX) > 0 ? 1 : -1;
         });
 
         Iterator it = arr.iterator();
