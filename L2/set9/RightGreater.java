7ed976b28f4e1626629ad86a5125355a97d1ca09
Vicky <81636126+Vigneshh25@users.noreply.github.com>
2023-04-09 19:53:43 +0530

    Create RightGreater.java

---
+++ L2/set9/RightGreater.java
@@ -0,0 +1,20 @@
+class RightGreater {
+    public int[] replaceElements(int[] arr) {
+        if(arr.length==0) return arr;
+        int max = arr[arr.length-1];
+        arr[arr.length-1]=-1;
+
+        for(int i=arr.length-2; i>=0; i--){
+            if(arr[i]> max){
+                int m=arr[i];
+                arr[i]=max;
+                max=m;
+            }
+            else{
+                arr[i]=max;
+            }
+        }
+        System.gc(); 
+        return arr;
+    }
+}
