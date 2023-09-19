1a82addc3a2003a8662a75988e55aa18642a9998
rameshk <rameshk@nmsworks.co.in>
2023-09-19 14:46:03 +0530

    dd

---
+++ L2/set1_0/MoveNegativesToEnd.java
@@ -0,0 +1,27 @@
+package L2.set1_0;
+
+public class MoveNegativesToEnd {
+    public static void main(String[] args) {
+        int[] arr = {1, -2, 3, -3, 1, 71, -64};
+        moveNegativesToEnd(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void moveNegativesToEnd(int[] arr) {
+        int n = arr.length;
+        int negIndex = 0;
+
+        // Traverse the array and move negative numbers to the end
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= 0) {
+                // If the element is non-negative, swap it with the first negative number found
+                int temp = arr[i];
+                arr[i] = arr[negIndex];
+                arr[negIndex] = temp;
+                negIndex++;
+            }
+        }
+    }
+}
---
+++ L2/set1_0/MoveZerosToEnd.java
@@ -0,0 +1,29 @@
+package L2.set1_0;
+
+public class MoveZerosToEnd {
+
+    public static void moveZeros(int[] nums) {
+        int nonZeroIndex = 0; // Points to the index where the next non-zero element should be placed.
+
+        // Iterate through the array.
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                // Swap the non-zero element with the element at nonZeroIndex.
+                int temp = nums[i];
+                nums[i] = nums[nonZeroIndex];
+                nums[nonZeroIndex] = temp;
+                nonZeroIndex++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 2, 0, 3, 4, 0, 5};
+        moveZeros(nums);
+
+        // Print the modified array.
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
