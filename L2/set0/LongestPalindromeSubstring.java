59f6dfb60ae2285f52893e6762578326f58035e9
rameshk <rameshk@nmsworks.co.in>
2023-10-19 13:54:33 +0530

    dff

--- L2/set0/LongestPalindromeSubstring.java
+++ L2/set0/LongestPalindromeSubstring.java
@@ -30,4 +30,29 @@ public class LongestPalindromeSubstring {
 
     }
 
+    public String longestPalindrome(String s) {
+        if(s==null) return null;
+        String longest = s.substring(0,1);
+        for(int i=0; i<s.length(); i++){
+            //odd case
+            String pal = expand(s,i,i);
+            if(pal.length()>longest.length()){
+                longest = pal;
+            }
+            pal = expand(s,i,i+1);
+            if(pal.length()>longest.length()){
+                longest = pal;
+            }
+        }
+        return longest;
+    }
+
+    static String expand(String s, int left, int right){
+        if(left>right) return null;
+        while(left>=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+        return s.substring(left+1, right);
+    }
 }
